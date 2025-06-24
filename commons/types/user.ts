d80595c1be75daf6d9269cfc60325d867ed92468
maruyu998 <maruyu998@gmail.com>
2025-06-24 10:05:03 +0900

    fix: update UserId and UserName schemas to use correct brand types

--- commons/types/user.ts
+++ commons/types/user.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const UserIdSchema = z.string().brand<"MoauthUserId">();
+export type UserIdBrandType = "YmwcUserId";
+export const UserIdSchema = z.string().brand<UserIdBrandType>();
 export type UserIdType = z.infer<typeof UserIdSchema>;
-export const UserNameSchema = z.string().brand<"MoauthUserName">();
+export type UserNameBrandType = "YmwcUserName";
+export const UserNameSchema = z.string().brand<UserNameBrandType>();
 export type UserNameType = z.infer<typeof UserNameSchema>;
